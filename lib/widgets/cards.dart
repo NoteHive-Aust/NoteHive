e88771e59294d0e18fd416a589d3aa4ba820beef
Mushfiqur Rahman <mushfiq.r156@gmail.com>
2026-08-20 02:26:40 +0600

    Add reusable notifications card in cards.dart

--- lib/widgets/cards.dart
+++ lib/widgets/cards.dart
@@ -15,9 +15,7 @@ class JoinedRoomCard extends StatelessWidget {
     return ListTile(
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(16),
-        side: BorderSide(
-          color: Color(0xFF352E60).withOpacity(0.1),
-        ),
+        side: BorderSide(color: Color(0xFF352E60).withOpacity(0.1)),
       ),
       tileColor: Colors.white,
       contentPadding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
@@ -55,20 +53,14 @@ class MyUploadsCard extends StatelessWidget {
   final String title;
   final String subtitle;
 
-  const MyUploadsCard({
-    super.key,
-    required this.title,
-    required this.subtitle,
-  });
+  const MyUploadsCard({super.key, required this.title, required this.subtitle});
 
   @override
   Widget build(BuildContext context) {
     return ListTile(
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(16),
-        side: BorderSide(
-          color: Color(0xFF352E60).withOpacity(0.1),
-        ),
+        side: BorderSide(color: Color(0xFF352E60).withOpacity(0.1)),
       ),
       tileColor: Colors.white,
       contentPadding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
@@ -100,10 +92,67 @@ class MyUploadsCard extends StatelessWidget {
           color: Color(0xFF352E60).withOpacity(0.6),
         ),
       ),
-      trailing: Icon(
-        Icons.chevron_right,
-        size: 24,
-        color: Color(0xFF1A1730),
+      trailing: Icon(Icons.chevron_right, size: 24, color: Color(0xFF1A1730)),
+    );
+  }
+}
+
+class NotificationsCard extends StatefulWidget {
+  final String title;
+  final String subtitle;
+  final String time;
+
+  const NotificationsCard({
+    super.key,
+    required this.title,
+    required this.subtitle,
+    required this.time,
+  });
+
+  @override
+  State<NotificationsCard> createState() => _NotificationsCardState();
+}
+
+class _NotificationsCardState extends State<NotificationsCard> {
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: Container(
+        width: 40,
+        height: 40,
+        decoration: BoxDecoration(
+          color: Color(0xFFE6E3FC).withOpacity(0.6),
+          shape: BoxShape.circle,
+        ),
+        child: Icon(
+          Icons.notifications_none,
+          size: 20,
+          color: Color(0xFF352E60),
+        ),
+      ),
+      title: Text(
+        widget.title,
+        style: TextStyle(
+          fontSize: 15,
+          fontWeight: FontWeight.w600,
+          color: Color(0xFF1A1730),
+        ),
+      ),
+      subtitle: Text(
+        widget.subtitle,
+        style: TextStyle(
+          fontSize: 13,
+          fontWeight: FontWeight.w400,
+          color: Color(0xFF1A1730),
+        ),
+      ),
+      trailing: Text(
+        widget.time,
+        style: TextStyle(
+          fontSize: 13,
+          fontWeight: FontWeight.w400,
+          color: Color(0xFF1A1730),
+        ),
       ),
     );
   }
