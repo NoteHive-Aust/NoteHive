68552fe8c91aaaf2cd280808f54720cee1b2f342
Mushfiqur Rahman <mushfiq.r156@gmail.com>
2026-08-21 00:15:44 +0600

    Create Announcements Screen (Without any functionality))

---
+++ lib/Screens/room_announcement_page.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import '../widgets/cards.dart';
+
+class RoomAnnouncementPage extends StatefulWidget {
+  const RoomAnnouncementPage({super.key});
+
+  @override
+  State<RoomAnnouncementPage> createState() => _RoomAnnouncementPageState();
+}
+
+class _RoomAnnouncementPageState extends State<RoomAnnouncementPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: appBar(),
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: ListView.separated(
+          padding: EdgeInsets.only(top: 45, bottom: 100, left: 20, right: 20),
+          itemCount: 10,
+          separatorBuilder: (context, index) {
+            return Divider(
+              height: 1,
+              thickness: 1,
+              indent: 18,
+              endIndent: 18,
+              color: Color(0xFF352E60).withOpacity(0.08),
+            );
+          },
+          itemBuilder: (context, index) {
+            return NotificationsCard(
+              title:
+                  'Mid term timetable has just been posted. Check the announcements.',
+              subtitle: '',
+              time: '1h ago',
+            );
+          },
+        ),
+      ),
+    );
+  }
+
+  AppBar appBar() {
+    return AppBar(
+      backgroundColor: Colors.white,
+      leadingWidth: 70,
+      titleSpacing: 10,
+      leading: Padding(
+        padding: const EdgeInsets.only(left: 20),
+        child: IconButton.outlined(
+          iconSize: 30,
+          onPressed: () {},
+          icon: Icon(Icons.chevron_left),
+          style: IconButton.styleFrom(
+            foregroundColor: Color(0xFF1A1730),
+            shape: const CircleBorder(),
+          ),
+        ),
+      ),
+      title: Text(
+        'Announcements',
+        style: TextStyle(
+          fontSize: 20,
+          fontWeight: FontWeight.w500,
+          color: Color(0xFF1A1730),
+        ),
+      ),
+    );
+  }
+}
