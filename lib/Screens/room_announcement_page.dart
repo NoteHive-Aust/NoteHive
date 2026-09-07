1a861d4f451057f5cfe4accc95d475874f40b26c
Mushfiqur Rahman <mushfiq.r156@gmail.com>
2026-09-07 23:48:47 +0600

    Announcement page and  my uploads back button fixed

--- lib/Screens/room_announcement_page.dart
+++ lib/Screens/room_announcement_page.dart
@@ -49,7 +49,9 @@ class _RoomAnnouncementPageState extends State<RoomAnnouncementPage> {
         padding: const EdgeInsets.only(left: 20),
         child: IconButton.outlined(
           iconSize: 30,
-          onPressed: () {},
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
           icon: Icon(Icons.chevron_left),
           style: IconButton.styleFrom(
             foregroundColor: Color(0xFF1A1730),
