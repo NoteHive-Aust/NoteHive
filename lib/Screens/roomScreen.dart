0cb2e4895062f25801fcf9cb376a581322e8245b
Mohammed Shaheer Imam <imshaheer2004@gmail.com>
2026-09-23 23:29:06 +0600

    upload by category

--- lib/Screens/roomScreen.dart
+++ lib/Screens/roomScreen.dart
@@ -49,7 +49,7 @@ class _RoomScreenState extends State<RoomScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       floatingActionButton: isModeratorUpload
-          ? floatingUploadButton(context: context, roomId: widget.roomId,isAdmin: false)
+          ? floatingUploadButton(context: context, roomId: widget.roomId,isAdmin: false,categories: widget.room.categories)
           : null,
       appBar: AppBar(
         leadingWidth: 70,
